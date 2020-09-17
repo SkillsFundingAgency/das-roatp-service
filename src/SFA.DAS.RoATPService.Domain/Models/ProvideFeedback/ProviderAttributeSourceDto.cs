99faa940543dc07830e780b33ef28b2e51ef0ae1
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-09-17 11:21:20 +0100

    Add dto for provide feedback service

---
+++ src/SFA.DAS.RoATPService.Domain/Models/ProvideFeedback/ProviderAttributeSourceDto.cs
@@ -0,0 +1,9 @@
+namespace SFA.DAS.RoATPService.Domain.Models.ProvideFeedback
+{
+    public class ProviderAttributeSourceDto
+    {
+        public string Name { get; set; }
+
+        public int Value { get; set; }
+    }
+}
