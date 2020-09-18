777c84b6839f65c424a0f86c3694d4c51e8b93d1
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-09-18 09:12:16 +0100

    Update repository to return model instead of object

--- src/SFA.DAS.RoATPService.Application/Interfaces/IFatDataExportRepository.cs
+++ src/SFA.DAS.RoATPService.Application/Interfaces/IFatDataExportRepository.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using SFA.DAS.RoATPService.Domain.Models.FatDataExport;
 
 namespace SFA.DAS.RoATPService.Application.Interfaces
 {
     public interface IFatDataExportRepository
     {
-        Task<IEnumerable<object>> GetFatDataExport();
+        Task<IEnumerable<FatDataExportDto>> GetFatDataExport();
     }
 }
