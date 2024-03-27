d26c5a1557de8855d3c405f22623589c3ce8a36d
MarkFCain <mark.cain@digital.education.gov.uk>
2024-03-27 08:21:14 +0000

    CSP-1287 correction of sql scripts

--- src/SFA.DAS.RoATPService.Database/PostDeployment/UpdateTables.sql
+++ src/SFA.DAS.RoATPService.Database/PostDeployment/UpdateTables.sql
@@ -1,19 +1,9 @@
 ﻿:r .\AddLookupForProviderTypes.sql
-
 :r .\AddLookupForOrganisationTypes.sql
-
 :r .\MapProviderTypesToOrganisationTypes.sql
-
 :r .\AddRemovedReasons.sql
-
 :r .\AddOrganisationStatus.sql
-
 :r .\AddProviderTypeOrganisationStatus.sql
-
-:r .\UpdateOrganisation.sql
-
 :r .\AddOrganisationCategory.sql
-
 :r .\AddOrganisationCategoryOrgTypeProviderType.sql
-
 :r .\AddOrganisationStatusEvent.sql
