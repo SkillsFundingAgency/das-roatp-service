b37582e1fb1a28b1104d53137ffb95f7ecd4cfbb
Mark Cain <31033361+MarkFCain@users.noreply.github.com>
2024-04-04 10:10:25 +0100

    Csp 1287 tidy up post deployment database scripts for das roatp service (#136)
    * CSP-1287 Made post-deployment database scripts tidy
    
    * CSP-1287 finalising post deployment scripts
    
    * CSP-1287 fix to update organisation
    
    * CSP-1287 moved organisations update directly into post deployment script
    
    * CSP-1287 removed unneeded item in script
    
    * CSP-1287 correction of sql scripts
    
    * CSP-1287 updated scripts based on TR
    
    ---------
    
    Co-authored-by: ManishAgarwalDfE <139247114+ManishAgarwalDfE@users.noreply.github.com>

---
+++ src/SFA.DAS.RoATPService.Database/PostDeployment/AddOrganisationStatus.sql
@@ -0,0 +1,36 @@
+﻿Go
+
+CREATE TABLE #TempOrganisationStatus(
+	[Id] [int],
+	[Status] [nvarchar](50),
+	[CreatedBy] [nvarchar](30),
+	[CreatedAt] [datetime2](7),
+	EventDescription [nvarchar](20) 
+);
+
+
+INSERT INTO #TempOrganisationStatus
+	([Id], [Status], [CreatedBy], [CreatedAt],[EventDescription])
+	VALUES
+	(0, 'Removed', 'System', SYSDATETIME(),'REMOVED'),
+	(1, 'Active', 'System', SYSDATETIME(),'ACTIVE'),
+	(2, 'Active - but not taking on apprentices', 'System', SYSDATETIME(),'ACTIVENOSTARTS'),
+	(3, 'On-boarding', 'System', getdate(),'INITIATED');
+
+	
+SET IDENTITY_INSERT [OrganisationStatus] ON;
+MERGE [OrganisationStatus] TARGET
+USING #TempOrganisationStatus SOURCE ON TARGET.Id=SOURCE.Id
+WHEN MATCHED THEN
+	UPDATE SET
+		TARGET.[Status] = SOURCE.[Status],
+		TARGET.[EventDescription] = SOURCE.[EventDescription]
+WHEN NOT MATCHED BY TARGET THEN 
+	INSERT ([Id], [Status], [CreatedBy], [CreatedAt],[EventDescription])
+	VALUES (SOURCE.[Id], SOURCE.[Status], SOURCE.[CreatedBy], SOURCE.[CreatedAt], SOURCE.[EventDescription]);
+
+SET IDENTITY_INSERT [OrganisationStatus] OFF;
+
+DROP TABLE #TempOrganisationStatus;
+
+Go
---
+++ src/SFA.DAS.RoATPService.Database/PostDeployment/UpdateTables.sql
@@ -0,0 +1,8 @@
+﻿:r .\AddLookupForProviderTypes.sql
+:r .\AddLookupForOrganisationTypes.sql
+:r .\MapProviderTypesToOrganisationTypes.sql
+:r .\AddRemovedReasons.sql
+:r .\AddOrganisationStatus.sql
+:r .\AddProviderTypeOrganisationStatus.sql
+:r .\AddOrganisationCategory.sql
+:r .\AddOrganisationCategoryOrgTypeProviderType.sql
