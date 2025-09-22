7e3a1ba8ebe30ca5c7e8a7d164075994a25af5a2
Deven Shah <devenshah1974@gmail.com>
2025-09-22 16:42:12 +0100

    CSP-2094 Restored old style authentication

--- src/SFA.DAS.RoATPService.Application.Api/AppStart/AddAuthenticationExtension.cs
+++ src/SFA.DAS.RoATPService.Application.Api/AppStart/AddAuthenticationExtension.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SFA.DAS.Api.Common.AppStart;
 using SFA.DAS.Api.Common.Configuration;
 using SFA.DAS.Api.Common.Infrastructure;
 using SFA.DAS.RoATPService.Application.Api.Extensions;
+using SFA.DAS.RoATPService.Settings;
 
 namespace SFA.DAS.RoATPService.Application.AppStart;
 
@@ -13,8 +16,11 @@ public static class AddAuthenticationExtension
     public const string RoATPServiceInternalAPI = "RoATPServiceInternalAPI";
     public static IServiceCollection AddAuthentication(this IServiceCollection services, IConfiguration configuration)
     {
+        var apiAuthentication = configuration.GetSection("ApiAuthentication").Get<ApiAuthentication>();
         if (!configuration.IsLocalEnvironment())
         {
+            AddOldStyleAuthentication(services, apiAuthentication);
+
             var azureAdConfiguration = configuration
                 .GetSection("AzureAd")
                 .Get<AzureActiveDirectoryConfiguration>()!;
@@ -30,4 +36,25 @@ public static class AddAuthenticationExtension
 
         return services;
     }
+
+    private static void AddOldStyleAuthentication(this IServiceCollection services, ApiAuthentication configuration)
+    {
+        services
+            .AddAuthentication(o => { o.DefaultScheme = JwtBearerDefaults.AuthenticationScheme; })
+            .AddJwtBearer(o =>
+            {
+                o.Authority = $"https://login.microsoftonline.com/{configuration.TenantId}";
+                o.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    RoleClaimType = "http://schemas.microsoft.com/ws/2008/06/identity/claims/role",
+                    ValidAudiences = configuration.Audience.Split(',')
+                };
+                o.Events = new JwtBearerEvents()
+                {
+                    OnTokenValidated = context => { return Task.FromResult(0); }
+                };
+            });
+    }
 }
+
+
