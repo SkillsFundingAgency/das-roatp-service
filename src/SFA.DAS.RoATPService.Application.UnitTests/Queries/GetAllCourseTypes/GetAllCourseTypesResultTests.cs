c901bc0651654ceaf1999b758bf62a3b7fd32c47
Afzal Hussain <afi259@hotmail.co.uk>
2025-10-13 09:56:00 +0100

    CSP-2258 Unit test updated

--- src/SFA.DAS.RoATPService.Application.UnitTests/Queries/GetAllCourseTypes/GetAllCourseTypesResultTests.cs
+++ src/SFA.DAS.RoATPService.Application.UnitTests/Queries/GetAllCourseTypes/GetAllCourseTypesResultTests.cs
@@ -1,9 +1,8 @@
-﻿using NUnit.Framework;
+﻿using FluentAssertions;
+using NUnit.Framework;
 using SFA.DAS.RoATPService.Application.Queries.GetAllCourseTypes;
 using SFA.DAS.RoATPService.Domain.Entities;
 using SFA.DAS.Testing.AutoFixture;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace SFA.DAS.RoATPService.Application.UnitTests.Queries.GetCourseTypes;
 public class GetAllCourseTypesResultTests
@@ -13,17 +12,11 @@ public class GetAllCourseTypesResultTests
     public void GetAllCourseTypesResult_MapsCourseTypeDataCorrectly(CourseType courseType)
     {
         // Arrange
-        var sut = new GetAllCourseTypesResult
-        {
-            CourseTypes = new List<CourseTypeSummary> { courseType }
-        };
-        ;
+        CourseTypeSummary sut = courseType;
+
         // Act & Assert
-        Assert.Multiple(() =>
-        {
-            Assert.That(courseType.Id, Is.EqualTo(sut.CourseTypes.FirstOrDefault().Id));
-            Assert.That(courseType.Name, Is.EqualTo(sut.CourseTypes.FirstOrDefault().Name));
-            Assert.That(courseType.LearningType, Is.EqualTo(sut.CourseTypes.FirstOrDefault().LearningType));
-        });
+        sut.Id.Should().Be(courseType.Id);
+        sut.Name.Should().Be(courseType.Name);
+        sut.LearningType.Should().Be(courseType.LearningType);
     }
 }
