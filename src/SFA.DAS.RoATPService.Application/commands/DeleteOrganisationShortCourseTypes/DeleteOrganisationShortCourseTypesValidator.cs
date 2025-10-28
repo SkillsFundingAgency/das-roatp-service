be42ba2d1619070246e4c0b6a9ded617e37f5a55
Deven Shah <devenshah1974@gmail.com>
2025-10-28 16:45:52 +0000

    CSP-2240 Refactored delete course types to use shared validation

--- src/SFA.DAS.RoATPService.Application/commands/DeleteOrganisationShortCourseTypes/DeleteOrganisationShortCourseTypesValidator.cs
+++ src/SFA.DAS.RoATPService.Application/commands/DeleteOrganisationShortCourseTypes/DeleteOrganisationShortCourseTypesValidator.cs
@@ -1,19 +1,14 @@
 ﻿using FluentValidation;
 using SFA.DAS.RoATPService.Application.Common.Validators;
-using SFA.DAS.RoATPService.Domain.Repositories;
 
 namespace SFA.DAS.RoATPService.Application.commands.DeleteOrganisationShortCourseTypes;
 public class DeleteOrganisationShortCourseTypesValidator : AbstractValidator<DeleteOrganisationShortCourseTypesCommand>
 {
-    public const string UkprnIsRequiredMessage = "Ukprn must not be empty";
-
-    public DeleteOrganisationShortCourseTypesValidator(IOrganisationsRepository organisationRepository)
+    public DeleteOrganisationShortCourseTypesValidator()
     {
         RuleFor(c => c.Ukprn)
-            .GreaterThan(0)
-            .WithMessage(UkprnIsRequiredMessage);
-        RuleFor(c => c.Ukprn)
-            .IsValidUkprnFormat()
-            .When(c => c.Ukprn > 0);
+            .Cascade(CascadeMode.Stop)
+            .UkprnNotEmpty()
+            .MustBeValidUkprnFormat();
     }
 }
