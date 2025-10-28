0daa64f906582995c347b1a6ab8c433d283dff7d
Deven Shah <devenshah1974@gmail.com>
2025-10-28 17:08:13 +0000

    CSP-2240 Added controller action for patch organisation

--- src/SFA.DAS.RoATPService.Api.UnitTests/Controllers/OrganisationsControllerTests/OrganisationsControllerPatchOrganisationTests.cs
+++ src/SFA.DAS.RoATPService.Api.UnitTests/Controllers/OrganisationsControllerTests/OrganisationsControllerPatchOrganisationTests.cs
@@ -3,57 +3,60 @@ using System.Threading;
 using System.Threading.Tasks;
 using AutoFixture.NUnit3;
 using MediatR;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.RoATPService.Application.Api.Controllers;
-using SFA.DAS.RoATPService.Application.Api.Models;
-using SFA.DAS.RoATPService.Application.Commands.UpdateOrganisationCourseTypes;
+using SFA.DAS.RoATPService.Application.Commands.PatchOrganisation;
 using SFA.DAS.RoATPService.Application.Common.Models;
 using SFA.DAS.RoATPService.Application.Mediatr.Behaviors;
 using SFA.DAS.Testing.AutoFixture;
 
-namespace SFA.DAS.RoATPService.Api.UnitTests.Controllers.OrganisationCourseTypesControllerTests;
-
-public class UpdateCourseTypesTests
+namespace SFA.DAS.RoATPService.Api.UnitTests.Controllers.OrganisationsControllerTests;
+public class OrganisationsControllerPatchOrganisationTests
 {
     [Test, MoqAutoData]
-    public async Task UpdateCourseTypes_ValidResponse_ReturnsNoContent(
+    public async Task PatchOrganisation_ValidResponse_ReturnsNoContent(
         [Frozen] Mock<IMediator> mediatorMock,
-        [Greedy] OrganisationCourseTypesController sut,
+        [Greedy] OrganisationsController sut,
         int ukprn,
-        UpdateCourseTypesModel model)
+        JsonPatchDocument<PatchOrganisationModel> model,
+        string userId,
+        CancellationToken cancellationToken)
     {
         // Arrange
         ValidatedResponse<SuccessModel> validatedResponse = new(new SuccessModel(true));
 
         mediatorMock
-            .Setup(m => m.Send(It.IsAny<UpdateOrganisationCourseTypesCommand>(), It.IsAny<CancellationToken>()))
+            .Setup(m => m.Send(It.IsAny<PatchOrganisationCommand>(), It.IsAny<CancellationToken>()))
             .ReturnsAsync(validatedResponse);
 
         // Act
-        var result = await sut.UpdateCourseTypes(ukprn, model, CancellationToken.None);
+        var result = await sut.PatchOrganisation(ukprn, model, userId, cancellationToken);
 
         // Assert
         Assert.That(result, Is.InstanceOf<NoContentResult>());
     }
 
     [Test, MoqAutoData]
-    public async Task UpdateCourseTypes_InvalidResponse_ReturnsBadRequestWithErrors(
+    public async Task PatchOrganisation_InvalidResponse_ReturnsBadRequestWithErrors(
         [Frozen] Mock<IMediator> mediatorMock,
-        [Greedy] OrganisationCourseTypesController sut,
+        [Greedy] OrganisationsController sut,
         int ukprn,
-        UpdateCourseTypesModel model,
-        List<ValidationError> errors)
+        JsonPatchDocument<PatchOrganisationModel> model,
+        string userId,
+        List<ValidationError> errors,
+        CancellationToken cancellationToken)
     {
         // Arrange
         var validatedResponse = new ValidatedResponse<SuccessModel>(errors);
         mediatorMock
-            .Setup(m => m.Send(It.IsAny<UpdateOrganisationCourseTypesCommand>(), It.IsAny<CancellationToken>()))
+            .Setup(m => m.Send(It.IsAny<PatchOrganisationCommand>(), It.IsAny<CancellationToken>()))
             .ReturnsAsync(validatedResponse);
 
         // Act
-        var result = await sut.UpdateCourseTypes(ukprn, model, CancellationToken.None);
+        var result = await sut.PatchOrganisation(ukprn, model, userId, cancellationToken);
 
         // Assert
         var badRequestResult = result as BadRequestObjectResult;
@@ -62,20 +65,23 @@ public class UpdateCourseTypesTests
     }
 
     [Test, MoqAutoData]
-    public async Task UpdateCourseTypes_UkprnNotFound_ReturnsNotFound(
+    public async Task PatchOrganisation_UkprnNotFound_ReturnsNotFound(
         [Frozen] Mock<IMediator> mediatorMock,
-        [Greedy] OrganisationCourseTypesController sut,
+        [Greedy] OrganisationsController sut,
         int ukprn,
-        UpdateCourseTypesModel model)
+        JsonPatchDocument<PatchOrganisationModel> model,
+        string userId,
+        List<ValidationError> errors,
+        CancellationToken cancellationToken)
     {
         // Arrange
         var validatedResponse = new ValidatedResponse<SuccessModel>(new SuccessModel(false));
         mediatorMock
-            .Setup(m => m.Send(It.IsAny<UpdateOrganisationCourseTypesCommand>(), It.IsAny<CancellationToken>()))
+            .Setup(m => m.Send(It.IsAny<PatchOrganisationCommand>(), It.IsAny<CancellationToken>()))
             .ReturnsAsync(validatedResponse);
 
         // Act
-        var result = await sut.UpdateCourseTypes(ukprn, model, CancellationToken.None);
+        var result = await sut.PatchOrganisation(ukprn, model, userId, cancellationToken);
 
         // Assert
         var badRequestResult = result as NotFoundResult;
---
+++ src/SFA.DAS.RoATPService.Application.UnitTests/Commands/PatchOrganisation/PatchOrganisationCommandTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using SFA.DAS.RoATPService.Application.Commands.PatchOrganisation;
+
+namespace SFA.DAS.RoATPService.Application.UnitTests.Commands.PatchOrganisation;
+public class PatchOrganisationCommandTests
+{
+    [Test]
+    public void ConvertsOrganisationToPatchOrganisationModel()
+    {
+        //Arrange
+        var organisation = new Domain.Entities.Organisation
+        {
+            Status = Domain.Entities.OrganisationStatus.Active,
+            RemovedReasonId = null,
+            ProviderType = Domain.Entities.ProviderType.Main,
+            OrganisationTypeId = 1
+        };
+        //Act
+        PatchOrganisationModel model = organisation;
+        //Assert
+        Assert.AreEqual(organisation.Status, model.Status);
+        Assert.AreEqual(organisation.RemovedReasonId, model.RemovedReasonId);
+        Assert.AreEqual(organisation.ProviderType, model.ProviderType);
+        Assert.AreEqual(organisation.OrganisationTypeId, model.OrganisationTypeId);
+    }
+}
