edb8abe132dca7c3a2b231c33b00eaf660921b1f
Deven Shah <devenshah1974@gmail.com>
2025-11-14 12:31:28 +0000

    CSP-2314 Added searchTerm to organisations endpoint

--- src/SFA.DAS.RoATPService.Application/Queries/GetOrganisations/GetOrganisationsQueryHandler.cs
+++ src/SFA.DAS.RoATPService.Application/Queries/GetOrganisations/GetOrganisationsQueryHandler.cs
@@ -1,11 +1,14 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using SFA.DAS.RoATPService.Application.Common;
+using SFA.DAS.RoATPService.Domain.Entities;
 using SFA.DAS.RoATPService.Domain.Repositories;
 
 namespace SFA.DAS.RoATPService.Application.Queries.GetOrganisations;
+
 public class GetOrganisationsQueryHandler(
         IOrganisationsRepository _organisationRepository)
     : IRequestHandler<GetOrganisationsQuery, GetOrganisationsQueryResult>
@@ -13,10 +16,15 @@ public class GetOrganisationsQueryHandler(
     public async Task<GetOrganisationsQueryResult> Handle(GetOrganisationsQuery request,
         CancellationToken cancellationToken)
     {
-        var organisations = await _organisationRepository.GetOrganisations(cancellationToken);
+        List<Organisation> organisations = await
+            (string.IsNullOrWhiteSpace(request.SearchTerm)
+            ? _organisationRepository.GetOrganisations(cancellationToken)
+            : _organisationRepository.GetOrganisationsBySearchTerm(request.SearchTerm, cancellationToken));
 
         var organisationsReturned = new GetOrganisationsQueryResult
-        { Organisations = organisations.Select(o => (OrganisationModel)o).ToList() };
+        {
+            Organisations = organisations.Select(o => (OrganisationModel)o).ToList()
+        };
 
         return organisationsReturned;
     }
