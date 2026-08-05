48a61cb0f31e435f3c0a3b4aeac006059734f0c1
Peter.Williams <peter.david.williams.1991@gmail.com>
2026-08-05 11:31:18 +0100

    CSP-2503 - Upgrade to .net 10.0, update packages to remove vulnerable/deprecated packages

--- src/SFA.DAS.RoATPService.Application.Api/Program.cs
+++ src/SFA.DAS.RoATPService.Application.Api/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi;
 using Newtonsoft.Json.Converters;
 using SFA.DAS.Api.Common.Infrastructure;
 using SFA.DAS.RoATPService.Application.Api.AppStart;
